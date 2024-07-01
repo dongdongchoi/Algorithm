767b44757ac7b9ea325781d9b67e3e75e9d58e50
dongdongchoi <39136303+dongdongchoi@users.noreply.github.com>
2024-07-01 17:37:43 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.11 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class Solution {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int time = 0;
+        int sum = 0;
+        Queue<Integer> q = new LinkedList<>();
+        for(int i =0; i<truck_weights.length;i++){
+            while (true){
+                if(q.isEmpty()){
+                    q.add(truck_weights[i]);
+                    time++;
+                    sum+=truck_weights[i];
+                    break;
+                } else if (q.size()==bridge_length) {
+                    sum-=q.poll();
+                }
+                else {
+                    if(sum+truck_weights[i]<=weight){
+                        q.add(truck_weights[i]);
+                        sum+=truck_weights[i];
+                        time++;
+                        break;
+                    }
+                    else{
+                        q.add(0);
+                        time++;
+                    }
+                }
+            }
+        }
+        
+        int answer = time+bridge_length;
+        return answer;
+    }
+}
