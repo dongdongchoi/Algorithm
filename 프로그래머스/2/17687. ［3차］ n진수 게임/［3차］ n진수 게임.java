8adea995653ffa5f8899a4965f3de04eef19218a
dongdongchoi <39136303+dongdongchoi@users.noreply.github.com>
2024-11-20 22:23:51 +0900

    [level 2] Title: [3차] n진수 게임, Time: 471.99 ms, Memory: 372 MB -BaekjoonHub

---
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        String answer = "";
+        String temp  = "";
+        
+        for(int i=0; temp.length()<m*t;i++){
+            temp+=Integer.toString(i,n);
+        }
+        
+        for(int i =p-1; answer.length()<t;i+=m){
+            answer +=temp.charAt(i);
+        }
+        
+        return answer.toUpperCase();
+    }
+}
