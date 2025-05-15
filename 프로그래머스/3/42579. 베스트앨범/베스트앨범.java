8c4712d457d42bcd969c443eb1e278038c0ecfad
dongdongchoi <39136303+dongdongchoi@users.noreply.github.com>
2025-05-15 20:31:09 +0900

    [level 3] Title: 베스트앨범, Time: 11.04 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,36 +1,67 @@
 import java.util.*;
-
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        HashMap<String, Integer> genrePlayCount = new HashMap<>();
-        HashMap<String, List<int[]>> genreSongs = new HashMap<>();
+        List<Integer>answer1 = new ArrayList<>();
         
-        // 장르별 총 재생 수 계산 및 곡 정보 저장
-        for (int i = 0; i < genres.length; i++) {
-            genrePlayCount.put(genres[i], genrePlayCount.getOrDefault(genres[i], 0) + plays[i]);
-            genreSongs.putIfAbsent(genres[i], new ArrayList<>());
-            genreSongs.get(genres[i]).add(new int[]{i, plays[i]}); // {곡 인덱스, 재생 수}
+        // 장르별 재생횟수 해시만들기
+        HashMap<String, List<int[]>> ha = new HashMap<>();
+        for(int i=0; i<genres.length;i++){
+            List<int[]> pl = ha.getOrDefault(genres[i], new ArrayList<>());
+            pl.add(new int[]{plays[i],i});
+            ha.put(genres[i],pl);
+        }
+       
+        
+        // 해시 안에 있는 재생횟수 내림차수 만들기
+       for(List<int[]>pl : ha.values()){
+           Collections.sort(pl,(a,b) ->{
+               if (a[0]==b[0]) return a[1]-b[1];
+               return b[0]-a[0];
+           });
+       }
+        
+        
+        // 장르별 총 재생횟수 
+        HashMap<String, Integer> ha1 = new HashMap<>();
+        for(int i=0; i<genres.length;i++){
+            if(ha1.containsKey(genres[i])){
+                ha1.put(genres[i],ha1.get(genres[i])+plays[i]);
+            }
+            else{
+                ha1.put(genres[i],plays[i]);
+            }
+        }
+       
+        
+        //가장 큰녀석 이름별로 List에 넣기
+        
+        List<Map.Entry<String, Integer>> el =  new ArrayList<>(ha1.entrySet());
+        Collections.sort(el,new Comparator<Map.Entry<String, Integer>>(){
+            @Override
+            public int compare(Map.Entry<String, Integer> entry1, Map.Entry<String, Integer> entry2){
+                return entry2.getValue() - entry1.getValue();
+            }
+        });
+        
+        List<String>li = new ArrayList<>();
+        for(Map.Entry<String, Integer>e : el){
+            li.add(e.getKey());
         }
         
-        // 장르별 재생 수를 기준으로 정렬
-        List<Map.Entry<String, Integer>> sortedGenres = new ArrayList<>(genrePlayCount.entrySet());
-        sortedGenres.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+       
+        // 큰녀석에 해당하는 (정렬된)플레이횟수를 리스트에 차례로 넣기
         
-        List<Integer> answerList = new ArrayList<>();
-        
-        // 각 장르에서 가장 많이 재생된 곡 2개 선택
-        for (Map.Entry<String, Integer> entry : sortedGenres) {
-            String genre = entry.getKey();
-            List<int[]> songs = genreSongs.get(genre);
-            songs.sort((a, b) -> b[1] - a[1]); // 재생 수 기준으로 정렬
-            
-            // 최대 2개 곡 추가
-            for (int i = 0; i < Math.min(2, songs.size()); i++) {
-                answerList.add(songs.get(i)[0]); // 곡 인덱스 추가
+        for(String genre : li){
+            List<int[]> playsWithIndex = ha.get(genre);
+            if(playsWithIndex != null && !playsWithIndex.isEmpty()){
+                for(int i = 0; i < Math.min(playsWithIndex.size(), 2); i++){
+                    answer1.add(playsWithIndex.get(i)[1]); // int[] 쌍에서 인덱스(두 번째 요소)만 추가
+                }
             }
         }
         
-        // List를 int[]로 변환
-        return answerList.stream().mapToInt(i -> i).toArray();
+        
+        int[]answer = answer1.stream().mapToInt(Integer :: intValue).toArray();
+        return answer;
     }
 }
