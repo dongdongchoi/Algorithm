79ad0a24fa556b175b49878c432f5d7fe5ff8204
dongdongchoi <39136303+dongdongchoi@users.noreply.github.com>
2025-05-22 00:00:38 +0900

    [level 2] Title: H-Index, Time: 7.40 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,20 +2,28 @@ import java.util.*;
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
-        int h = 0;
-        Arrays.sort(citations);
-       for(int i=citations.length-1;i>=0;i--) {
-			h++;
-           if(citations[i]<h) {
-				answer = h;
-				break;
-			}
-			
-		}
-        if (answer == 0) {
-            answer = h;
-            return answer;
+        
+        for(int j = 0; j<citations.length -1; j++){
+            for(int i =0; i<citations.length-1;i++){
+            
+            if(citations[i]>citations[i+1]){
+                int temp = citations[i];
+                citations[i] = citations[i+1];
+                citations[i+1] = temp;
+            }
         }
-        return answer-1;
+        }
+        int length = citations.length;
+        
+        
+        for(int i =0; i< citations.length;i++){
+            if(citations[i]>=length - i){
+                System.out.println(i);
+                answer = length - i;
+                break;
+            }
+        }
+       
+        return answer;
     }
 }
