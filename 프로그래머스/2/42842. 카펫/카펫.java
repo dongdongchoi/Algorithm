315309fec781cd478aa24a01746c53c63b79cf59
dongdongchoi <39136303+dongdongchoi@users.noreply.github.com>
2025-06-04 20:23:12 +0900

    [level 2] Title: 카펫, Time: 10.01 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,23 +1,19 @@
-import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
-        
-        ArrayList<Integer>list = new ArrayList<>();
-        int target = brown+yellow;
-        int y =0;//가로
-        
-        for(int i=1; i<=target;i++){ // i는 세로
-            if(target%i==0){
-                y=target / i;
-                if(i>=y&&(y-2)*(i-2)== yellow){
-                    list.add(i);
-                    list.add(y);
-                    break;  
+        int[] answer = new int[2];
+        int by = brown + yellow;
+            
+            for(int i = 3; i<=by; i++ ){
+                if(by%i==0){
+                    int y = by/i;
+                    int x = by/y;
+                    if((x-2)*(y-2) == yellow){
+                        answer[0] = x;
+                        answer[1] = y;
+                    }
                 }
             }
-        }
-        
-        int[] answer = list.stream().mapToInt(i->i).toArray();
+            
         return answer;
     }
 }
